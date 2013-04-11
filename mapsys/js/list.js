220ca19b2af4f2d2a30d5ae99096d5e263992d2d
Little Black <wuze000@gmail.com>
2013-04-11 22:09:45 +0800

    add ico

--- mapsys/js/list.js
+++ mapsys/js/list.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	
 		if( $(this).next(".sponsors_down").css('display') !='none' )
 		{
-			$(this).next(".sponsors_down").slideUp(1500);
+			$(this).next(".sponsors_down").slideUp('slow');
 			$(this).next(".sponsors_down").css('display','none');
 		}
 		else
